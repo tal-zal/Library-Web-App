9010722c075cf814125f3c8989e3a19870f91145
Tal Zaloilov <tzaloilov@tractionondemand.com>
2022-01-06 12:38:39 -0800

    Added book deletion, UI changes

--- script.js
+++ script.js
@@ -32,10 +32,47 @@ function populateCards(){
 
     for (let i = 0; i < myLibrary.length; i++){
         let book = myLibrary[i];
-        let bookHTMLObject = document.createElement('div');
-        bookHTMLObject.classList.add("bookCard")
+
+        let title = book.title;
+        let author = book.author;
+        let numPages = book.numPages;
+        myLibrary[i].arrayIndex = i; //updates index variable of each book object for deletion purposes
+
+        var cardDisplay = document.querySelector(".cardDisplay");
+        
+        let bookHTMLObject = document.createElement("div");
+        bookHTMLObject.classList.add("bookCard");
+        
+        let deleteButton = document.createElement("div");
+        deleteButton.innerText = "x";
+        deleteButton.classList.add("deleteButton");
+        deleteButton.addEventListener("click", deleteBook);
+
+        let titleHTML = document.createElement("p");
+        let authorHTML = document.createElement("p");
+        let numPagesHTML = document.createElement("p");
+
+        titleHTML.textContent = title;
+        authorHTML.textContent = author;
+        numPagesHTML.textContent = numPages;
+
+        bookHTMLObject.appendChild(deleteButton);
+        bookHTMLObject.appendChild(titleHTML);
+        bookHTMLObject.appendChild(authorHTML);
+        bookHTMLObject.appendChild(numPagesHTML);
+
         cardDisplay.appendChild(bookHTMLObject);
     }
+}
+
+function deleteBook(index){
+    var cardDisplay = document.querySelector(".cardDisplay");
+    let nodeList = cardDisplay.childNodes;
+    nodeList[index].remove();
+}
+
+function reIndexBooks(){
+    
 }
 
 function addCardPopulate(){
@@ -51,6 +88,15 @@ function addCardPopulate(){
     let bookHTMLObject = document.createElement("div");
     bookHTMLObject.classList.add("bookCard");
     
+    let deleteButton = document.createElement("div");
+    deleteButton.innerText = "x";
+    deleteButton.classList.add("deleteButton");
+    deleteButton.addEventListener("click", () => {
+        deleteBook(book.arrayIndex);
+        myLibrary.splice(book.arrayIndex, 1);
+        reIndexBooks();
+    })
+
     let titleHTML = document.createElement("p");
     let authorHTML = document.createElement("p");
     let numPagesHTML = document.createElement("p");
@@ -59,6 +105,7 @@ function addCardPopulate(){
     authorHTML.textContent = author;
     numPagesHTML.textContent = numPages;
 
+    bookHTMLObject.appendChild(deleteButton);
     bookHTMLObject.appendChild(titleHTML);
     bookHTMLObject.appendChild(authorHTML);
     bookHTMLObject.appendChild(numPagesHTML);
